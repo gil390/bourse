094dafd16e874bc8aae26b069b3d0728cac7db43
Gilles B <gnetwb@hotmail.com>
2023-10-29 16:56:28 +0100

    script d essai

---
+++ check_ETF.sh
@@ -0,0 +1,17 @@
+PYDIR="/home/gilles/Téléchargements/venv/bin"
+
+${PYDIR}/python3 src/bourse.py QS0009079318
+
+#${PYDIR}/python3 src/bourse.py QS0009079318 "AMUNDI LABEL EQUILIBRE SOLIDAIRE ESR - F" &
+#${PYDIR}/python3 src/bourse.py QS0009080720 "AMUNDI LABEL MONETAIRE ESR - F" &
+#${PYDIR}/python3 src/bourse.py QS0009080738 "AMUNDI LABEL PRUDENCE ESR - F" &
+#${PYDIR}/python3 src/bourse.py QS0009084177 "AMUNDI OPPORTUNITES ESR - F" &
+#${PYDIR}/python3 src/bourse.py QS0009084201 "AMUNDI LABEL OBLIGATAIRE ESR - F" &
+#${PYDIR}/python3 src/bourse.py QS0009084227 "AMUNDI LABEL ACTIONS EUROLAND ESR - F" &
+#${PYDIR}/python3 src/bourse.py QS0009097047 "THALES OBLIGATIONS" &
+#${PYDIR}/python3 src/bourse.py QS0009097054 "THALES ACTIONS EUROMONDE" &
+#${PYDIR}/python3 src/bourse.py QS0009099829 "AMUNDI PROTECT 90 ESR - F" &
+#${PYDIR}/python3 src/bourse.py IWDA.AS "iShares Core MSCI World UCITS ETF USD" &
+#${PYDIR}/python3 src/bourse.py VUSA.AS "Vanguard S&P 500" &
+#${PYDIR}/python3 src/bourse.py VWCE.DE "Vanguard FTSE All-World" &
+#${PYDIR}/python3 src/bourse.py SXR8.DE "Vanguard FTSE All-World" &
