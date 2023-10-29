ffa6c1e9a57b7130c0e64f1630a0fc49ded0baf3
Gilles B <gnetwb@hotmail.com>
2023-10-29 21:25:47 +0100

    version multiframes verticales

--- src/bourse.py
+++ src/bourse.py
@@ -5,38 +5,48 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 from tkinter import *
 
+class SFrame(Frame):
+    def __init__(self, parent, figure):
+        super().__init__(parent)
+        self.configure(background='#FF0000')
+        canvas = FigureCanvasTkAgg(figure,
+                               master = self)
+        toolbar = NavigationToolbar2Tk(canvas,
+                                   self)
+        toolbar.update()
+        canvas.get_tk_widget().pack(expand=1, fill="both")
+
 class TheApp():
-    def __init__(self, argsobj):
-        window = Tk()
-        window.title('Plotting in Tkinter')
-        window.geometry("1024x768")
+    def __init__(self, isin_codes):
+        self._window = Tk()
+        self._window.resizable(True, True)
+        self._window.columnconfigure(0, weight=1)
+
+        self._window.title('Plotting in Tkinter')
+        #self._window.geometry("1024x768")
 
         # je force un backend qui n est pas Tk
         plt.switch_backend('agg')
 
-        get_yfinance(argsobj[1], None if len(argsobj) == 2 else argsobj[2])
-        fig = plt.figure(num=1)
-        canvas = FigureCanvasTkAgg(fig,
-                               master = window)
-        toolbar = NavigationToolbar2Tk(canvas,
-                                   window)
-        toolbar.update()
-        canvas.get_tk_widget().pack()
-        window.mainloop()
+        print(type(isin_codes))
+        therow = 0
+        for code in isin_codes:
+            self._window.rowconfigure(therow, weight=1)
+            fig = get_yfinance(code)
+            sframe = SFrame(self._window, fig)
+            sframe.grid(row = therow, column = 0, sticky = "NSEW")
+            therow += 1
+
+        self._window.mainloop()
         plt.close('all')
 
-def get_yfinance(isin_code, nom = None):
-    subtitle = "Code: " + isin_code
-    if type(nom) != type(None):
-        subtitle += " Nom: " + nom
-
+# @return current figure
+def get_yfinance(isin_code):
     msft = yf.Ticker(isin_code)
     hist = msft.history(period="5y")
     indicator = tti.indicators.IchimokuCloud(hist)
     # print(f"Signal for {sys.argv[1]} : {indicator.getTiSignal()}")
     plt = indicator.getTiGraph()
-    plt.suptitle(subtitle)
-    #plt.show()
-    return plt
+    return plt.gcf()
 
-TheApp(sys.argv)
+TheApp(sys.argv[1:])
